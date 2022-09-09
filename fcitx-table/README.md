5d4220f45db4092e9a3057aeae2be42f36c3c78c
Yanying Wang <yanyingwang1@gmail.com>
2022-09-10 04:06:35 +0800

    zm-to-mzm and data

---
+++ fcitx-table/README.md
@@ -0,0 +1,3 @@
+
+
+https://fcitx-im.org/wiki/How_to_make_your_own_table-based_input_method
---
+++ zm-to-mzm.rkt
@@ -0,0 +1,60 @@
+#!/usr/bin/env racket
+#lang racket
+
+(define (to-mzm str)
+  (match-define (list code cn) (string-split str))
+  (define code/lst (string->list code))
+  (define ncode/lst (for/list ([c code-lst]
+                               #:do [(define cnum (- (char->integer c) 96))])
+                      (hash-ref tables cnum)))
+  (define ncode (list->string ncode/lst))
+  (string-join (list ncode cn))
+  )
+
+(define tables
+  (hash 8 #\q
+        7 #\w
+        6 #\e
+        5 #\r
+        13 #\t
+        14 #\y
+        15 #\u
+        16 #\i
+        17 #\o
+        18 #\p
+        4 #\a
+        3 #\s
+        2 #\d
+        1 #\f
+        19 #\g
+        9 #\h
+        10 #\j
+        11 #\k
+        12 #\l
+        23 #\z
+        22 #\x
+        21 #\c
+        20 #\v
+        24 #\b
+        25 #\n
+        26 #\m
+        ))
+
+(define zm-port
+  (open-input-file "data-zm.txt"))
+(to-mzm (read-line zm-port))
+
+
+#|
+-- -- -- --  ++ ++  -- -- -- --
+-- -- -- --  ++ ++  -- -- -- ;;
+-- -- -- --  ++ ++  -- .< .> /?
+
+08 07 06 05  13 14  15 16 17 18
+04 03 02 01  19 09  10 11 12 ;;
+23 22 21 20  24 25  26 .< .> /?
+
+Q- W- E- R-  +T Y+  U- I- O- P-
+A- S- D- F-  +G H+  J- K- L- ;;
+Z- X- C- V-  +B N+  M- .< .> /?
+|#
